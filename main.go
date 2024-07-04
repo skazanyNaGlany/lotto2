7ef5c0a66781efa48014f237a7e3b20d4eabd4c0
Pawel <screamingbox@gmail.com>
2024-07-04 05:39:29 +0200

    Save

--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"sort"
 )
 
 func main() {
-	sequence := []int{11, 14, 34, 35, 36, 43}
-	nextNumbers := generateNextNumbers(sequence, 6) // Generate 3 next numbers
-	fmt.Println(nextNumbers)
+	// lotto step 1
+	sequence := []int{4, 12, 20, 23, 24, 49}
+	nextNumbers := generateNextNumbers(sequence, 6, 1, 49)
+	printResults(nextNumbers, 6)
+
+	nextNumbers = generateNextNumbers(sequence, 6, 2, 49)
+	printResults(nextNumbers, 6)
+
+	nextNumbers = generateNextNumbers(sequence, 6, 3, 49)
+	printResults(nextNumbers, 6)
+
+	// lotto plus step 1
+	sequence = []int{8, 9, 19, 22, 23, 43}
+	nextNumbers = generateNextNumbers(sequence, 6, 1, 49)
+	printResults(nextNumbers, 6)
+
+	// lotto plus step 2
+	nextNumbers = generateNextNumbers(sequence, 6, 2, 49)
+	printResults(nextNumbers, 6)
+
+	// lotto plus step 3
+	nextNumbers = generateNextNumbers(sequence, 6, 3, 49)
+	printResults(nextNumbers, 6)
 }
 
-func generateNextNumbers(sequence []int, count int) []int {
+func printResults(sequence []int, count int) {
+	source := make([]int, count)
+	target := make([]int, count)
+
+	copy(source, sequence[:count])
+	copy(target, sequence[count:])
+
+	sort.Ints(source)
+	sort.Ints(target)
+
+	log.Printf("%v -> %v", source, target)
+}
+
+func generateNextNumbers(sequence []int, count int, step int, maxNumber int) []int {
 	for i := 0; i < count; i++ {
 		lastNumber := sequence[len(sequence)-1]
 		// Simple strategy: Use a variable step based on the sequence's behavior
 		// This is a placeholder logic. You might need a specific pattern or rule.
-		step := (sequence[1] - sequence[0] + sequence[2] - sequence[1] + sequence[3] - sequence[2]) / 3
+		step := (sequence[1] - sequence[0] + sequence[2] - sequence[1] + sequence[3] - sequence[2]) / step
 		nextNumber := lastNumber + step
-		if nextNumber > 49 {
-			nextNumber = nextNumber % 49 // Ensure the number is within 1-49
+		if nextNumber > maxNumber {
+			nextNumber = nextNumber % maxNumber // Ensure the number is within 1-49
 		}
 		sequence = append(sequence, nextNumber)
 	}
